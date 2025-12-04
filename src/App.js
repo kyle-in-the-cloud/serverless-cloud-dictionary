5b246c6bda763fe4e8b5a18a853aaee18a0315b6
Kyle Michael <kmichael1496@gmail.com>
2025-12-04 14:23:43 -0800

    Integrate API Gateway with React front-emd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [filteredTerms, setFilteredTerms] = useState([]);
 
-  const apiUrl = '<ENTER_YOUR_API_URL>'; // Replace with your API Gateway URL
+  const apiUrl = 'https://lnwheefod5.execute-api.us-east-1.amazonaws.com/dev'; // Replace with your API Gateway URL
 
   const handleSearch = () => {
     console.log('Fetching data from API...');
